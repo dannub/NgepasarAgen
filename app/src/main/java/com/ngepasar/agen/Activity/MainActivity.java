17ed9e211f353d1c83a59aeb6002b975a92c2e31
dannub <fardannubimantara@gmail.com>
2020-06-27 20:48:09 +0700

    Membuat Project

---
+++ app/src/main/java/com/ngepasar/agen/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.ngepasar.agen.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ngepasar.agen.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
